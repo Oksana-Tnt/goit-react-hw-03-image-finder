752d496bf98a80a06ed15b1df90470f0644441fe
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-09-21 16:34:04 +0200

    update task2

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,44 +1,65 @@
 import { getImage } from 'Services/GetImage';
+import ErrorCard from 'components/ErrorCard/ErrorCard';
 import { Component } from 'react';
 
+const STATUS = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 class ImageGallery extends Component {
   state = {
     images: null,
-    isLoading: false,
-    error:""
+    error: '',
+    status: STATUS.IDLE,
   };
-  async componentDidUpdate(prevProps, prevState) {
-    
+  componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchText !== this.props.searchText) {
-      this.setState({ isLoading: true });
-      try {
-        const data = await getImage(this.props.searchText);       
-        this.setState({ images: data.data.hits });
-      } catch (error) {
-        this.setState({error: error.message});
-      }
+      this.setState({ status: STATUS.PENDING });
 
-      this.setState({ isLoading: false });
+      getImage(this.props.searchText)
+        .then(data => {
+          console.log(data);
+          if (data.status === 200)
+            this.setState({ images: data.data.hits, status: STATUS.RESOLVED });
+          else return data.statusText;
+        })
+        .catch(error => {
+          this.setState({ error: error.message, status: STATUS.REJECTED });
+        });
+
+      // try {
+      //   const data = await getImage(this.props.searchText);
+      //   this.setState({ images: data.data.hits });
+      // } catch (error) {
+      //   this.setState({ error: error.message });
+      // }
     }
   }
-  render() {
-    const { images, isLoading } = this.state;
 
-    return (
-      <>
-        {isLoading && (
-          <div className="spinner-border text-success" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>
-        )}
-        <ul>
-          {images &&
-            images.map(el => {
+  render() {
+    const { images, error, status } = this.state;
+
+    if (status === STATUS.PENDING)
+      return (
+        <div className="spinner-border text-success" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      );
+    else if (status === STATUS.RESOLVED) {
+      return (
+        <>
+          <ul>
+            {images.map(el => {
               return <li key={el.id}>{el.tags}</li>;
             })}
-        </ul>
-      </>
-    );
+            <button>Load</button>
+          </ul>
+        </>
+      );
+    } else if (status === STATUS.REJECTED)
+      return <ErrorCard>{error}</ErrorCard>;
   }
 }
 
